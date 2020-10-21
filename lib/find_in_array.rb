49fdc3508978119800ac66df6dc8ba65e45cc6de
Riley Iverson <rmiverson99@gmail.com>
2020-10-21 23:39:11 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,12 +1,10 @@
 def find_element_index(array, value_to_find)
   i = 0
-  puts array[array.length - 1]
   while array.length > i do
     if array[i] == value_to_find
       return i
     end
     i += 1
   end
-  
   return nil
 end
